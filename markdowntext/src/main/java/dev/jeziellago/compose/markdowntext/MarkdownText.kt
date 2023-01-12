a58d493f3dbb33097cc5cbdb3feb7d636c008ab1
Yash Agarwal <yash786agg@gmail.com>
2023-01-12 15:54:55 +0200

    Update markdowntext/src/main/java/dev/jeziellago/compose/markdowntext/MarkdownText.kt
    Co-authored-by: Jeziel Lago <jeziellago@gmail.com>

--- markdowntext/src/main/java/dev/jeziellago/compose/markdowntext/MarkdownText.kt
+++ markdowntext/src/main/java/dev/jeziellago/compose/markdowntext/MarkdownText.kt
@@ -47,7 +47,7 @@ fun MarkdownText(
     // it also enable the parent view to receive the click event
     disableLinkMovementMethod: Boolean = false,
     imageLoader: ImageLoader? = null,
-    linkUrl: ((String) -> Unit)? = null
+    onLinkClicked: ((String) -> Unit)? = null
 ) {
     val defaultColor: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
     val context: Context = LocalContext.current
