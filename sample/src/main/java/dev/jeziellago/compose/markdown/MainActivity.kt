50e15a8e6004cf2edcf9821c3e29155b2f37b3b4
Jeziel Lago <jeziellago@gmail.com>
2024-01-05 14:47:26 -0300

    Update readme

--- sample/src/main/java/dev/jeziellago/compose/markdown/MainActivity.kt
+++ sample/src/main/java/dev/jeziellago/compose/markdown/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
